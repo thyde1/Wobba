c278c550fd374745e3e29f861254d8ee7cdc5000
Tom Hyde <thyde@neworbit.co.uk>
2020-07-02 18:30:13 +0100

    Added death sound

--- Wobba/src/game/character/DeathChecker.cpp
+++ Wobba/src/game/character/DeathChecker.cpp
@@ -6,11 +6,14 @@
 void DeathChecker::update(int elapsed)
 {
     if (this->gameObject->globalPosition.y > 100 || !this->gameObject->getComponent<CharacterInfo>()->alive) {
+        auto deathSoundPlayer = new SoundPlayer("assets/Death.wav");
         this->gameObject->game->instantiateObject()
+            ->addComponent(deathSoundPlayer)
             ->addRenderer(new DeadRenderer())
             ->addRenderer(new TextRenderer(SDL_Color{ 0, 0, 0, 255 }, TextAlignment::CENTER, std::vector<const char *>{"YOU ARE DEAD", "Press space to try again"}))
             ->addInputHandler(new DeadInputHandler())
             ->setGlobalPosition({ (double)(this->gameObject->game->windowSize.w / 20), (double)(this->gameObject->game->windowSize.h / 20 ) - 10});
+        deathSoundPlayer->play();
         this->gameObject->game->destroyObject(this->gameObject);
     }
 }
